3b103b84595cf4b5b0de8fcf175d0adf31225f19
Ken Moini <ken@kenmoini.com>
2019-11-01 22:48:59 -0500

    stuffer is stuffed

--- cloudbees-core/ss-ca-stuffer.sh
+++ cloudbees-core/ss-ca-stuffer.sh
@@ -7,11 +7,17 @@ set -e
 
 ## check for custom keystore
 CUSTOM_TRUSTSTORE=$JENKINS_HOME/.cacerts
+
 if [ ! -f "$CUSTOM_TRUSTSTORE/cacerts" ]; then
+    echo "No local keystore found, creating..."
     mkdir -p $CUSTOM_TRUSTSTORE
     cp $JAVA_HOME/jre/lib/security/cacerts $CUSTOM_TRUSTSTORE
 fi
+
 chmod +w $CUSTOM_TRUSTSTORE/cacerts
+
+echo ""
+echo -e "Starting SSL Certificate import...\n"
 
 for CERT in "$@"
 do
@@ -20,14 +26,16 @@ do
   CERTNAME=${FILENAME//"."/"-"}
   #echo "Q" | openssl s_client -connect ${CERT} 2>/dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > /tmp/$FILENAME.pem
   keytool -printcert -rfc -sslServer ${CERT} > /tmp/$FILENAME.pem
-  echp "Importing certificate to keystore..."
+  echo "Importing certificate to keystore..."
   keytool -list -storepass changeit -keystore $CUSTOM_TRUSTSTORE/cacerts -alias $CERTNAME
   if [ $? -eq 0 ]; then
     echo "Certificate already exists, skipping..."
   else
     echo "Certificate not in keystore, importing..."
-    keytool -import -noprompt -storepass changeit -file /tmp/$FILENAME.pem -alias $CERTNAME -keystore $CUSTOM_TRUSTSTORE/cacerts 2>/dev/null
+    keytool -import -noprompt -storepass changeit -file /tmp/$FILENAME.pem -alias $CERTNAME -keystore $CUSTOM_TRUSTSTORE/cacerts
   fi
+  echo ""
 done
 
 chmod -w $CUSTOM_TRUSTSTORE/cacerts
+echo -e "Finished importing certificates!\n"
