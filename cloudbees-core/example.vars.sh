acf1a3d7369f70e1e2a5777bbdfa7f0c9da660e6
Ken Moini <ken@kenmoini.com>
2019-11-04 09:41:58 -0600

    add example.vars.sh

---
+++ cloudbees-core/example.vars.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+OCP_HOST=""
+OCP_USERNAME=""
+OCP_PASSWORD=""
+
+OCP_CREATE_PROJECT="true"
+OCP_PROJECT_NAME="cicd-pipeline"
+
+OCP_CJOC_ROUTE="cjoc.ocp.example.com"
+OCP_CJOC_ROUTE_EDGE_TLS="true"
+
+OC_ARG_OPTIONS=""
+CBC_OCP_WORK_DIR="/tmp/cbc-ocp"
+INTERACTIVE="false"
